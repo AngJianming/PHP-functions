d2ddb85f1f44879bafec34a35bd066dce7787c78
AngJianming <angjianming2005@hotmail.com>
2024-11-14 22:02:18 +0800

    updates

---
+++ Basic Functions/1.php
@@ -0,0 +1,13 @@
+<!-- Creating a function -->
+<?php
+function myMessage() {
+    echo "Hello world!";
+}
+
+// Calling a function 
+myMessage();
+
+?>
+
+
+
---
+++ Basic Functions/2.php
@@ -0,0 +1,5 @@
+<!-- Functional Arguments -->
+<?php
+
+?>
+
