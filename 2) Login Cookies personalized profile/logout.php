76f488eb64a433503d587d0d711f59ec46550406
Ang Jianming <angjianming2005@hotmail.com>
2025-02-06 12:45:22 +0800

    Delete 2) Login Cookies personalized profile directory

--- 2) Login Cookies personalized profile/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start the session
-session_start();
-
-// Destroy the session
-session_destroy();
-
-echo "<script>alert('You have been logged out.');window.location.href='login.php';</script>";
-?>
